d9b2787739f5043306a31285d83f3c569223566d
Klaus Kazlauskas <klaus.machado@stormsec.com.br>
2016-10-27 11:38:56 -0200

    NumberPicker

--- app/src/main/java/br/com/klauskpm/thatsnewstome/NumberPickerPreference.java
+++ app/src/main/java/br/com/klauskpm/thatsnewstome/NumberPickerPreference.java
@@ -5,6 +5,7 @@ import android.preference.DialogPreference;
 import android.util.AttributeSet;
 import android.util.Log;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.NumberPicker;
 
 /**
@@ -12,11 +13,47 @@ import android.widget.NumberPicker;
  */
 
 public class NumberPickerPreference extends DialogPreference {
+    private final static String ANDROID_NS = "http://schemas.android.com/apk/res/android";
+    private final static int INT_NOT_DEFINED = -999;
 
     private NumberPicker numberPicker;
+    private boolean mMaxValueWasSet = false;
+    private boolean mMinValueWasSet = false;
+
+    private int mMaxValue;
+    private int mMinValue;
+
+    private int mDefaultValue;
+
+    private String mPrefKey;
+    private int mPrefValue;
 
     public NumberPickerPreference(Context context, AttributeSet attrs) {
         super(context, attrs);
+
+        int prefKeyResourceId = attrs.getAttributeResourceValue(ANDROID_NS, "key", 0);
+        mPrefKey = context.getString(prefKeyResourceId);
+
+        int maxValue = attrs.getAttributeIntValue(null, "maxValue", 0);
+        setMaxValue(maxValue);
+
+        int minValue = attrs.getAttributeIntValue(null, "minValue", 0);
+        setMinValue(minValue);
+
+        int defaultResourceId = attrs.getAttributeResourceValue(ANDROID_NS, "defaultValue", 0);
+        int defaultValue = Integer.parseInt(context.getString(defaultResourceId));
+
+        if (defaultValue < minValue || defaultValue == 0)
+            defaultValue = minValue;
+        mDefaultValue = defaultValue;
+
+        Log.d("NumberPickerPreference", "NumberPickerPreference: " + mDefaultValue);
+
+        int prefValue = getSharedPreferences().getInt(mPrefKey, INT_NOT_DEFINED);
+        if (prefValue == INT_NOT_DEFINED)
+            prefValue = mDefaultValue;
+
+        mPrefValue = prefValue;
     }
 
     @Override
@@ -26,8 +63,13 @@ public class NumberPickerPreference extends DialogPreference {
 
     public NumberPicker generateNumberPicker() {
         numberPicker = new NumberPicker(getContext());
-        numberPicker.setMinValue(1);
-        numberPicker.setMaxValue(5);
+        if (mMinValueWasSet)
+            numberPicker.setMinValue(mMinValue);
+
+        if (mMaxValueWasSet)
+            numberPicker.setMaxValue(mMaxValue);
+
+        numberPicker.setValue(mPrefValue);
 
         /*
          * Anything else you want to add to this.
@@ -36,12 +78,29 @@ public class NumberPickerPreference extends DialogPreference {
         return numberPicker;
     }
 
+    public void setMaxValue(int mMaxValue) {
+        this.mMaxValue = mMaxValue;
+        this.mMaxValueWasSet = true;
+    }
+
+    public void setMinValue(int mMinValue) {
+        this.mMinValue = mMinValue;
+        this.mMinValueWasSet = true;
+    }
+
+    @Override
+    protected View onCreateView(ViewGroup parent) {
+        setSummary("" + mPrefValue);
+        return super.onCreateView(parent);
+    }
+
     @Override
     protected void onDialogClosed(boolean positiveResult) {
         super.onDialogClosed(positiveResult);
         if (positiveResult) {
-            int port = numberPicker.getValue();
-            Log.d("NumberPickerPreference", "NumberPickerValue : " + port);
+            mPrefValue = numberPicker.getValue();
+            setSummary("" + mPrefValue);
+            Log.d("NumberPickerPreference", "NumberPickerValue : " + mPrefValue);
         }
     }
 
